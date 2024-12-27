38e29f069ea26d065f8e18aa724c0e5276053871
raghav5196 <raghavpatidar251@gmail.com>
2024-12-27 13:20:41 +0530

    done

---
+++ arrays_and_collection/basic.py
@@ -0,0 +1,39 @@
+
+# basic of arrays and collection
+
+from array import *
+#
+# numbers = array('i',[10, 20, 30, 40, 50])
+# print(numbers[0])
+# print(numbers[4])
+
+
+# access the elements of array
+# name = ["raghav","Rochak","Shlok"]
+# r=name[0]
+# r2=name[1]
+# s=name[2]
+# print( r,'\n',r2,'\n',s)
+
+
+# insertions array
+
+# numbers =array('i',[10,20,30,40])
+# numbers.insert(3,10)
+# for x in numbers:
+#     print(x)
+
+
+# delete operation
+
+# numbers =array('i',[10,20,30,40])
+# numbers.remove(20)
+# for x in numbers:
+#     print(x)
+
+# update operation
+#
+# numbers =array('i',[10,20,30,40])
+# numbers[2]=100
+# for x in numbers:
+#     print(x)
